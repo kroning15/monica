a960e9a30c9817e8e335712054650ed6dd9d5801
Daniel Kroning <daniel.kroning@fkpscorpio.com>
2026-04-06 18:06:57 +0200

    Hide PHP deprecation notices on Vercel runtime

--- api/index.php
+++ api/index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
+// Prevent PHP deprecation notices from being rendered to end users on Vercel.
+if (getenv('VERCEL_ENV')) {
+    ini_set('display_errors', '0');
+    ini_set('display_startup_errors', '0');
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+}
+
 require __DIR__.'/../public/index.php';
