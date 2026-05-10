eee5ac44526e52d67e94a46c8ca9aff8c45eaa2c
Daniel Kroning <daniel.kroning@fkpscorpio.com>
2026-05-10 11:45:47 +0200

    Log full runtime exception details via global middleware

---
+++ app/Http/Middleware/LogUnhandledThrowable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
+
+class LogUnhandledThrowable
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            return $next($request);
+        } catch (Throwable $throwable) {
+            error_log(sprintf(
+                '[MiddlewareCatch] %s: %s in %s:%d (path=%s method=%s)',
+                $throwable::class,
+                $throwable->getMessage(),
+                $throwable->getFile(),
+                $throwable->getLine(),
+                $request->path(),
+                $request->method()
+            ));
+
+            throw $throwable;
+        }
+    }
+}
+
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureSignupIsEnabled;
+use App\Http\Middleware\LogUnhandledThrowable;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(LogUnhandledThrowable::class);
         $middleware->throttleApi();
         $middleware->validateCsrfTokens(except: [
             '/dav',
